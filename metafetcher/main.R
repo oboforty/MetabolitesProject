1e86ee034612ed73a919227afd101d77feb27a37
oboforty <rajmund.csombordi@gmail.com>
2020-06-16 12:14:45 +0200

    Thesis final commit

--- metafetcher/main.R
+++ metafetcher/main.R
@@ -1,5 +1,13 @@
 source('R/discover.R')
 
-resp <- resolve_single_id('pubchem_id', '16683874')
+#resp <- resolve_single_id('pubchem_id', '16683874')
+
+#df.out <- resp$df
+
+
+
+
+df.res <- read.csv("discovery.csv", stringsAsFactors=FALSE)
+resp <- resolve_metabolites(df.res)
 
 df.out <- resp$df
--- pyproto/tests/coverage.py
+++ pyproto/tests/coverage.py
@@ -3,50 +3,51 @@ import json
 import sys
 from collections import defaultdict
 
-file = '../../tmp/tests/resolve_dump_4.csv'
+file = '../../tmp/tests/resolve_dump_{}.csv'
 
 
 n_ambigous = defaultdict(int)
 n_resolved = defaultdict(int)
 n_missing = defaultdict(int)
+T = 0
 
 
-with open(file, encoding='utf-8') as csvfile:
-    dialect = csv.Sniffer().sniff(csvfile.readline())
-    dialect.escapechar = "\\"
+for file_id in range(1, 5):
+    with open(file.format(file_id), encoding='utf-8') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.readline())
+        dialect.escapechar = "\\"
 
-    reader = csv.DictReader(csvfile, delimiter='|', quotechar='"', dialect=dialect)
-    csvfile.seek(0)
-    T = 0
+        reader = csv.DictReader(csvfile, delimiter='|', quotechar='"', dialect=dialect)
+        csvfile.seek(0)
 
-    for row in reader:
-        T += 1
+        for row in reader:
+            T += 1
 
-        for attr, val in row.items():
-            if val[0] == '{' and val[-1] == '}':
-                val = '[' + val[1:-1] + ']'
+            for attr, val in row.items():
+                if val[0] == '{' and val[-1] == '}':
+                    val = '[' + val[1:-1] + ']'
 
-                try:
-                    val = json.loads(val)
-                except:
-                    if attr == 'names':
-                        continue
+                    try:
+                        val = json.loads(val)
+                    except:
+                        if attr == 'names':
+                            continue
 
-            if isinstance(val, list):
-                L = len([v for v in val if v != 'NA'])
-            elif type(val) in [str, int, float, bool]:
-                # this case doesn't actually happen because every value in the CSV is stored as an array
-                L = 1
-            else:
-                raise Exception(str(type(val)))
+                if isinstance(val, list):
+                    L = len([v for v in val if v != 'NA'])
+                elif type(val) in [str, int, float, bool]:
+                    # this case doesn't actually happen because every value in the CSV is stored as an array
+                    L = 1
+                else:
+                    raise Exception(str(type(val)))
 
-            if L == 0: n_missing[attr] += 1
-            elif L == 1: n_resolved[attr] += 1
-            else: n_ambigous[attr] += 1
+                if L == 0: n_missing[attr] += 1
+                elif L == 1: n_resolved[attr] += 1
+                else: n_ambigous[attr] += 1
 
-            # if '_id' in attr:
-            #    # attr is a reference ID
-            #    pass
+                # if '_id' in attr:
+                #    # attr is a reference ID
+                #    pass
 
 keys = list(sorted(n_resolved.keys()))
 print('\t'.join(keys))
